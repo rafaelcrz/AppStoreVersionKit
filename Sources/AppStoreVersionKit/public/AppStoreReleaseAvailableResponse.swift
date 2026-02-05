3e1c80159bdac131167170e6a6ae9246fb4d41a4
Rafael Felipe <rafael.felipe1989@gmail.com>
2026-02-04 22:35:16 -0300

    Rename trackName to appName in AvailableRelease struct and update coding keys for decoding

--- Sources/AppStoreVersionKit/public/AppStoreReleaseAvailableResponse.swift
+++ Sources/AppStoreVersionKit/public/AppStoreReleaseAvailableResponse.swift
@@ -6,13 +6,19 @@ public struct AppStoreReleaseAvailableResponse: Codable {
     public struct AvailableRelease: Codable {
         public let version: String?
         public let releaseNotes: String?
-        public let trackName: String?
+        public let appName: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case version
+            case releaseNotes
+            case appName = "trackName"
+        }
         
         public init(from decoder: any Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             version = try container.decodeIfPresent(String.self, forKey: .version)
             releaseNotes = try container.decodeIfPresent(String.self, forKey: .releaseNotes)
-            trackName = try container.decodeIfPresent(String.self, forKey: .trackName)
+            appName = try container.decodeIfPresent(String.self, forKey: .appName)
         }
     }
 }
